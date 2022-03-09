957c055437546349528b082e03991980d02dfa35
Wijitha <wijitha.wijenayake@webjet.com.au>
2022-03-09 18:12:25 +0530

    Added reactstrap

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 
 export function Login() {
-  return <div>This is login</div>
+  return <div>This is login
+
+    <Button color="primary">Login</Button>
+  </div>
 }

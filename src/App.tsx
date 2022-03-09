4403f3bc05cac20264de5c6e3359fb94fa10ba66
Wijitha <wijitha.wijenayake@webjet.com.au>
2022-03-09 18:04:19 +0530

    Added boostrap template

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,40 @@ import { Login } from './features/auth/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <div>
-          <h1>Bookkeeper</h1>
-          <nav
-            style={{
-              borderBottom: "solid 1px",
-              paddingBottom: "1rem",
-            }}
-          >
-            <Link to="/login">Invoices</Link> |{" "}
-            <Link to="/counter">Expenses</Link>
-          </nav>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="#">Start Bootstrap</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item"><Link className="nav-link active" to="/login">Login</Link></li>
+              <li className="nav-item"><Link className="nav-link active" to="/counter">Counter</Link></li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
-      </header>
-
-      <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/counter" element={<Counter/>}/>
-      </Routes>
-
-    </div>
+      </nav>
+      <div className="container">
+        <div className="text-center mt-5">
+          <h1>A Bootstrap 5 Starter Template</h1>
+          <p className="lead">A complete project boilerplate built with Bootstrap</p>
+          <p>Bootstrap v5.1.3</p>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/counter" element={<Counter />} />
+          </Routes>
+        </div>
+      </div>
+    </>
   );
 }
 

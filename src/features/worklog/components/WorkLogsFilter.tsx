4b8932a9641666f5719f5444ff48b6673183806f
Wijitha <Wijithapaw@gmail.com>
2022-07-27 18:06:04 +0530

    Improved worklog filter

--- src/features/worklog/components/WorkLogsFilter.tsx
+++ src/features/worklog/components/WorkLogsFilter.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Card, CardBody, Col, Input, Modal, ModalBody, ModalHeader, Row } from "reactstrap";
+import { Button, Card, CardBody, Col, Form, FormGroup, Input, Modal, ModalBody, ModalHeader, Row } from "reactstrap";
 import { dateHelpers } from "../../../app/helpers";
 import { useAppDispatch, useAppSelector } from "../../../app/hooks";
 import DateSelect from "../../../components/DateSelect";
@@ -23,50 +23,64 @@ export default function WorkLogsFilter() {
 
   return <Card className="mt-2 mb-2">
     <CardBody>
-      <Row>
-        <Col>
-          <Row>
-            <Col>
-              <ProjectsMultiSelect selectedIds={worklogFilter.projects}
-                onSelect={(p) => handleFileterChange('projects', p)} />
-            </Col>
-          </Row>
-          <Row className="mt-2">
-            <Col>
-              <Input type="text" value={worklogFilter.searchTerm}
-                placeholder="Search Term"
-                name="searchTerm"
-                onChange={(e) => handleFileterChange(e.target.name, e.target.value)} />
-            </Col>
-            <Col xs={3}>
-              <DateSelect value={dateHelpers.toDate(worklogFilter.from)}
-                placeHolder="From"
-                onChange={(d) => handleFileterChange('from', dateHelpers.toIsoString(d))} />
-            </Col>
-            <Col xs={3}>
-              <DateSelect value={dateHelpers.toDate(worklogFilter.to)}
-                placeHolder="To"
-                onChange={(d) => handleFileterChange('to', dateHelpers.toIsoString(d))} />
-            </Col>
-          </Row>
-        </Col>
-        <Col md={2} className="pt-1">
-          <Row>
-            <Col>
-              <Button color="primary" className='w-100' onClick={() => setNewWorkLog(true)}>
-                New
-              </Button>
-            </Col>
-          </Row>
-          <Row className="mt-2">
-            <Col>
-              <Button color="primary" className='w-100' onClick={search}>
-                Search
-              </Button>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+      <Form>
+        <Row>
+          <Col>
+            <Row>
+              <Col>
+                <FormGroup>
+                  <ProjectsMultiSelect selectedIds={worklogFilter.projects}
+                    onSelect={(p) => handleFileterChange('projects', p)} />
+                </FormGroup>
+              </Col>
+              <Col md={4}>
+                <FormGroup>
+                  <Input type="text" value={worklogFilter.searchTerm}
+                    placeholder="Search Term"
+                    name="searchTerm"
+                    onChange={(e) => handleFileterChange(e.target.name, e.target.value)} />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <FormGroup>
+                  <DateSelect value={dateHelpers.toDate(worklogFilter.from)}
+                    placeHolder="From"
+                    onChange={(d) => handleFileterChange('from', dateHelpers.toIsoString(d))} />
+                </FormGroup>
+              </Col>
+              <Col>
+                <FormGroup>
+                  <DateSelect value={dateHelpers.toDate(worklogFilter.to)}
+                    placeHolder="To"
+                    onChange={(d) => handleFileterChange('to', dateHelpers.toIsoString(d))} />
+                </FormGroup>
+              </Col>
+            </Row>
+          </Col>
+          <Col md={2}>
+            <Row>
+              <Col>
+                <FormGroup>
+                  <Button color="primary" className='w-100' onClick={() => setNewWorkLog(true)}>
+                    New
+                  </Button>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <FormGroup>
+                  <Button color="primary" className='w-100' onClick={search}>
+                    Search
+                  </Button>
+                </FormGroup>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Form>
       <Modal size="lg" centered toggle={() => setNewWorkLog(!newWorkLog)} isOpen={newWorkLog}>
         <ModalHeader toggle={() => setNewWorkLog(!newWorkLog)}>New Work Log</ModalHeader>
         <ModalBody>
